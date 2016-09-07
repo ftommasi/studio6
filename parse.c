4c1a32e95f0335c8952b4f4fc3755cf880c1547a
Fausto G. Tommasi <mmasito123@yahoo.com>
2016-09-07 14:27:05 -0500

    Commit comment

--- parse.c
+++ parse.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -6,24 +7,32 @@ int main(int argc, char** argv){
   
   int max_args = 15;
   int max_argv_size = max_args + 2; //one for argv[0], one for null terminator
-  char* cmd;
+  char* cmd = (char*) malloc(sizeof(char)*256);
   char* my_argv[max_argv_size];
   
   fgets(buf,256,stdin);
   buf = strtok (buf,"\n");
   char* next = strtok(buf," ");
-  strcpy(next,cmd);
-  if(next == cmd){
-    printf("ERROR");
-    return -1;
+  strcpy(cmd,next);
+  int i=0;
+  while(next){
+    printf("in while appended %s\n",next);
+    my_argv[i] = (char*) malloc(sizeof(char)*256);
+    strcpy(my_argv[i],next);
+//    my_argv[i] = temp;
+    next = strtok(NULL," ");    
+    i++;
   }
-  else{
-    printf("%s",cmd);
+  int j=0;
+  printf("[ ");
+  for(j;j<i;j++){
+    printf("%s",my_argv[j]);
+    if(j+1 != i)
+      printf(", ");
+    else
+      printf(" ");
   }
-  while(next){    
-    printf("%s\n",next);
-    next = strtok(NULL," ");
-    
-  }
+  printf("]\n");
+  execvp(cmd,my_argv);
   return 0;
 }
